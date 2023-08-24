3c63c71b69d2d1991cc63c881b7bc19a1aad6284
Leonardo Silva <42047532+leoesilva@users.noreply.github.com>
2023-08-24 10:53:25 -0300

    fix: refatoração

--- exercicios/src/Aula02/PesquisaSalarios.java
+++ exercicios/src/Aula02/PesquisaSalarios.java
@@ -4,22 +4,31 @@ import javax.swing.*;
 
 public class PesquisaSalarios {
     public static void main(String[] args) {
-        String nome, sexo;
-        int idade, qtdHomem = 0, qtdMulher = 0;
+        String nome;
+        char sexo;
+        int idade, qtdHomem = 0, qtdMulher = 0, qtdEntrevistados = 0;
         float salario, total = 0;
         for(int i = 0; i < 5; i++) {
             nome = JOptionPane.showInputDialog("Digite o seu nome: ");
             idade = Integer.parseInt(JOptionPane.showInputDialog("Digite a sua idade: "));
-            sexo = JOptionPane.showInputDialog("Digite o seu sexo (M ou F): ");
+            sexo = Character.toUpperCase(JOptionPane.showInputDialog("Digite o seu sexo (M ou F): ").charAt(0));
             salario = Float.parseFloat(JOptionPane.showInputDialog("Digite o seu salário: "));
-            if (Character.toUpperCase(sexo.charAt(0)) == 'M') {
+            if (sexo == 'M') {
                 qtdHomem += 1;
+                qtdEntrevistados += 1;
             }
-            if (Character.toUpperCase(sexo.charAt(0)) == 'F') {
+            else if (sexo == 'F') {
                 qtdMulher += 1;
+                qtdEntrevistados += 1;
             }
-            total += salario;
+            else {
+                qtdEntrevistados += 1;
+            }
+            total += salario;                        
         }
-        JOptionPane.showMessageDialog(null, "Quantidade de homens: " + qtdHomem + "\nQuantidade de mulheres: " + qtdMulher + "\nSoma dos salários: R$ " + total);
+        JOptionPane.showMessageDialog(null, "Quantidade de homens: " + qtdHomem 
+        + "\nQuantidade de mulheres: " + qtdMulher 
+        + "\nQuantidade de entrevistados: " + qtdEntrevistados
+        + "\nSoma dos salários: R$ " + total);
     }
 }
