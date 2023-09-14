63bf46ebfee44ef60e99d13e3a5d50ef25aef2e1
Leonardo Silva <42047532+leoesilva@users.noreply.github.com>
2023-09-14 08:32:50 -0300

    feat: lista de exercícios

--- exercicios/src/Lista/Maior3Numeros.java
+++ exercicios/src/Lista/Maior3Numeros.java
@@ -2,18 +2,22 @@ package Lista;
 
 import javax.swing.JOptionPane;
 
-public class MaiorNumero {
+public class Maior3Numeros {
     public static void main(String[] args) {
-        int num1 = 0, num2 = 0;
+        int num1 = 0, num2 = 0, num3 = 0;
 
         num1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número:"));
         num2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número:"));
+        num3 = Integer.parseInt(JOptionPane.showInputDialog("Digite o terceiro número:"));
 
-        if (num1 > num2) {
+        if (num1 > num2 && num1 > num3) {
             JOptionPane.showMessageDialog(null, "O maior número é " + num1 + ".");
-        } else if (num1 < num2) {
+        } else if (num2 > num1 && num2 > num3) {
             JOptionPane.showMessageDialog(null, "O maior número é " + num2 + ".");
-        } else {
+        } else if (num3 > num1 && num3 > num2) {
+            JOptionPane.showMessageDialog(null, "O maior número é " + num3 + ".");
+        }
+         else {
             JOptionPane.showMessageDialog(null, "Foram digitados números iguais!");
         }
     }
